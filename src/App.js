ce7749f8185c9a9b5a1a0b34a74b62fe11a1424c
alfixed <alfixed@gmail.com>
2019-05-04 20:08:55 +0300

    add grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
+import snow from './snow.jpg'
+import owl from './owl.jpg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='app-wrapper'>
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
       </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <br />
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src={snow} />
+        </div>
+        <div>
+          <img src={owl} />
+        </div>
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
