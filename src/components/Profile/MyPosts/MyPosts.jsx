c9d2436fac7d474ba1d7f0e6efc594e89eeb53c8
alfixed <alfixed@gmail.com>
2019-05-06 16:52:31 +0300

    refactoring app

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,11 +4,15 @@ import Post from './Post/Post';
 
 const MyPosts = () => {
     return (
-        <div>
-            My post
+        <div className={s.postsBlock}>
+            <h3>My post</h3>
             <div>
-                <textarea></textarea>
-                <button>Add post</button>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
             <Post message='Hi, how are you?' likesCount='10'/>
             <Post message="It's my first post" likesCount='25'/>
