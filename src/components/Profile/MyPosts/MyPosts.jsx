3aa80bd2abb2ee491ad8895ae92a199d889c63b4
alfixed <alfixed@gmail.com>
2019-05-06 12:29:37 +0300

    add my posts components

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './MyPosts.module.css';
+import Post from './Post/Post';
+
+const MyPosts = () => {
+    return (
+        <div>
+            My post
+            <div>
+                <textarea></textarea>
+                <button>Add post</button>
+            </div>
+            <Post />
+            <Post />
+            <Post />
+        </div>
+    );
+}
+
+export default MyPosts;
