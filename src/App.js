b3c1968cfb0b01a087731cb8a7ffb979a0d192c6
alfixed <alfixed@gmail.com>
2019-05-06 11:01:56 +0300

    crate components

--- src/App.js
+++ src/App.js
@@ -1,55 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import snow from './snow.jpg'
-import owl from './owl.jpg'
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className='app-wrapper'>
-      <header className="header">
-        <img src={logo} className="logo" alt="logo" />
-      </header>
-      <nav className='nav'>
-        <div>
-          <a>Profile</a>
-        </div>
-        <div>
-          <a>Messages</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <br />
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav>
-      <div className='content'>
-        <div>
-          <img src={snow} />
-        </div>
-        <div>
-          <img src={owl} />
-        </div>
-        <div>
-          My post
-          <div>
-            New post
-          </div>
-          <div>
-            <div>
-              post 1
-            </div>
-            <div>
-              post 2
-            </div>
-          </div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import snow from '../snow.jpg'
+import owl from '../owl.jpg'
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img src={snow} />
+        </div>
+        <div>
+          <img src={owl} />
+        </div>
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
