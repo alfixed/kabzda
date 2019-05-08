960000cf1d393023f8c8494bef2c7755f0acdc94
alfixed <alfixed@gmail.com>
2019-05-08 13:46:12 +0300

    add redux

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,26 @@
 const ADD_POST = 'ADD_POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT';
 
-const profileReducer = (state, action) => {
+let initialState = {
+    posts: [
+        {
+            id: 1,
+            message: 'Hi, how are you?',
+            likesCount: 10
+        }, {
+            id: 2,
+            message: "It's my first post",
+            likesCount: 50
+        }, {
+            id: 3,
+            message: 'Yo',
+            likesCount: 2
+        }
+    ],
+    newPostText: ''
+}
+
+const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST:
             let newPost = {
