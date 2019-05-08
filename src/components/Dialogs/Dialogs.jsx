917597921bc1aaf12cea9fccf2150b4657d4a771
alfixed <alfixed@gmail.com>
2019-05-08 11:43:43 +0300

    add send message on dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,10 +2,33 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
+import {updateMessageTextCreator, sendMessageCreator} from '../../data/state';
 
 const Dialogs = (props) => {
-    let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
-    let messagesElements = props.state.messages.map(m => <Message message={m.message} id={m.id}/>);
+    let state = props
+        .store
+        .getState()
+        .dialogsPage;
+    let dialogsElements = state
+        .dialogs
+        .map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElements = state
+        .messages
+        .map(m => <Message message={m.message} id={m.id}/>);
+    let newMessageText = state.newMessageText;
+
+    let onSendMessageClick = () => {
+        props
+            .store
+            .dispatch(sendMessageCreator());
+    }
+
+    let onNewMessageText = (e) => {
+        let text = e.target.value;
+        props
+            .store
+            .dispatch(updateMessageTextCreator(text));
+    }
 
     return (
         <div className={s.dialogs}>
@@ -14,6 +37,17 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+                <div>
+                    <div>
+                        <textarea
+                            onChange={onNewMessageText}
+                            value={newMessageText}
+                            placeholder='Enter your message'></textarea>
+                    </div>
+                    <div>
+                        <button onClick={onSendMessageClick}>Send</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
