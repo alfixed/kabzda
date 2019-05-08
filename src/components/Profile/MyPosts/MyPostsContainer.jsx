ab1b4867b2daaf8f507f4700f623b878bd1284a9
alfixed <alfixed@gmail.com>
2019-05-08 16:13:22 +0300

    add context provider

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
 import MyPosts from './MyPosts';
 import {addPostCreator, updateNewPostTextCreator} from '../../../redux/profile-reducer';
+import StoreContext from '../../../StoreContext';
 
-const MyPostsContainer = (props) => {
-    let state = props
-        .store
-        .getState();
+const MyPostsContainer = () => {
+    return (
+        <StoreContext.Consumer>
+            {(store) => {
+                let state = store.getState();
+                let onAddPost = () => (store.dispatch(addPostCreator()));
 
-    let onAddPost = () => {
-        props
-            .store
-            .dispatch(addPostCreator());
-    }
+                let onPostChange = (text) => (store.dispatch(updateNewPostTextCreator(text)));
 
-    let onPostChange = (text) => {
-        props
-            .store
-            .dispatch(updateNewPostTextCreator(text));
-    }
-
-    return (<MyPosts
-        posts={state.profilePage.posts}
-        newPostText={state.newPostText}
-        updateNewPostText={onPostChange}
-        addPost={onAddPost}/>);
+                return <MyPosts
+                    posts={state.profilePage.posts}
+                    newPostText={state.newPostText}
+                    updateNewPostText={onPostChange}
+                    addPost={onAddPost}/>
+                }
+            }
+        </StoreContext.Consumer>
+    );
 }
 
 export default MyPostsContainer;
