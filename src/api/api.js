b4ab04b13c999e4f8aba773920eba408c7f07806
Федосимов Алексей Александрович <AFedosimov@alfabank.ru>
2019-06-11 14:43:53 +0300

    add axios create

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,17 @@
 import Axios from 'axios';
 
-export const getUsers = (pageNumber,pageSize) => {
-    return Axios
-        .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${pageSize}`, { withCredentials: true })
-        .then(responce => responce.data);
+const instance = Axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': '3ffdc20f-020a-4ffc-a3ab-8519eea24b10 '
+    }
+});
+
+export const usersAPI = {
+    getUsers(pageNumber, pageSize) {
+        return instance
+            .get(`users?page=${pageNumber}&count=${pageSize}`, { withCredentials: true })
+            .then(responce => responce.data);
+    }
 }
